d1e42a2ce4e5be2f09e60ced12aa31633b33421b
mung <anvo930524@gmail.com>
2021-03-05 22:38:21 +0900

    day4

--- C_Class/MyHeader.h
+++ C_Class/MyHeader.h
@@ -1,17 +1,17 @@
 #include <stdio.h>
-int f[101];
 
-int Fib(int a){
+int get_Fib(int a){
+    int f[101];
     if(a <= 2){
         f[a] = 1;
     }
     else if(f[a] == 0){
-        f[a] = Fib(a-1)+Fib(a-2);
+        f[a] = get_Fib(a-1)+get_Fib(a-2);
     }
     return f[a];
 }
 
-void Func(char k){
+void get_type(char k){
     if(k >= '0' && int(k) <= '9'){
         printf("숫자");
     }
@@ -25,4 +25,56 @@ void Func(char k){
         printf("특수문자");
     }
     printf("\n");
+}
+
+int get_Min(int *arr, int n){
+    int min_ = arr[0];
+    for(int i = 1; i < n; i++){
+        if(min_ > arr[i]){
+            min_ = arr[i];
+        }
+    }
+    return min_;
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int* int_Sort(int *arr, int n){
+    for(int i = 0; i < n-1; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[i] > arr[j]){
+                swap(arr+i, arr+j); // swap(&arr[i], &arr[j])과 동일
+            }
+        }
+    }
+    return arr;
+}
+
+int get_GCD(int a, int b){
+    int min_ = a > b ? b : a;
+    int max_ = a > b ? a : b;
+    while (min_ != 0)
+    {
+        int temp = max_;
+         max_ = min_;
+         min_ = temp % min_;
+    }
+    return max_;
+}
+
+int* get_CD(int a){
+    int arr[100];
+    int *ptr = arr;
+    int idx = 0;
+    for(int i = 1; i <= a; i++){
+        if(a%i == 0){
+            ptr[++idx] = i;
+        }
+    }
+    ptr[0] = idx;
+    return ptr;
 }
--- C_Class/tutorial.cpp
+++ C_Class/tutorial.cpp
@@ -2,6 +2,27 @@
 #include <string.h>
 #include "MyHeader.h"
 
+int get_Min(int *arr, int n);
+int* int_Sort(int *arr, int n);
+int get_GCD(int a, int b);
+int* get_CD(int a);
+
+typedef struct{
+    char name[20];
+    char phone[20];
+    int age;
+} person;
+
+typedef union{
+    int d1;
+    double d2;
+    char d3;
+} u_data;
+
+typedef enum{
+    mon, tue, wed, thu, fri, sat, sun
+} week;
+
 int main()
 {   
 /*  변수
@@ -112,11 +133,131 @@ int main()
         i++;
     }
 */
-/*  포인터
-*/
+/*  인덱싱
     char buffer[100];
     int key;
     scanf("%s", buffer); scanf("%d", &key);
     printf("%c", buffer[key]);
+*/
+/*  포인터
+    arr 자체를 변화시키지 않고 ex) arr++ or arr+1 은 X
+    arr의 위치를 컨트롤할 수 있는 *pArr를 pArr = arr로 배열의 주소를 받아서 pArr를 더하거나 빼거나 하는것을 추천
+
+    int a;
+    int *Pa = &a;
+    void *vp;
+    int arr[5] = {0,1,2,3,4};
+    int *pArr;
+    pArr = arr;
+
+    printf("a = %08x\n", &a);
+    printf("*Pa = %08x\n\n", Pa);
+
+    a = 1000;
+    printf("a = %d = %08x\n", a, &a);
+    printf("*Pa = %d = %08x\n\n", *Pa, Pa);
+    
+    a++;
+    printf("a = %d = %08x\n", a, &a);
+    printf("*Pa = %d = %08x\n\n", *Pa, Pa);
+    
+    vp = Pa++;
+    printf("a = %d = %08x\n", a, &a);
+    printf("*Pa = %d = %08x\n", *Pa, Pa);
+    printf("*vp= %08x\n\n", vp);
+    for(int i = 0; i < 5; i++){
+        printf("%d\n", pArr[i]);
+    }
+*/ 
+/*  포인터 매개변수
+    int n;
+    printf("배열 사이즈 입력 : ");
+    scanf("%d", &n);
+
+    int *arr, i;
+    for(i = 0; i < n; i++){
+        printf("%d번째 값 (%d/%d) : ", i+1, i+1, n);
+        scanf("%d", &arr[i]);
+    }
+    printf("배열의 최솟값 : %d\n\n", get_Min(arr, n));
+
+    arr = int_Sort(arr, n);
+    printf("정렬 후 배열\n");
+    for(i = 0; i < n; i++){
+        printf("%d번째 값 (%d/%d) : %d\n", i+1, i+1, n, arr[i]);
+    }
+
+    int a, b;
+    printf("최대 공약수를 구할 두 수를 입력하세요 : ");
+    scanf("%d %d", &a, &b);
+    int gcd = get_GCD(a, b);
+    printf("%d와 %d의 최대공약수는 %d\n", a, b, gcd);
+    printf("최대공약수 %d의 약수는 ", gcd);
+    int *cd = get_CD(gcd);
+    for(int i = 1; i <= *cd; i++){
+        printf("%d ", *(cd+i));
+    }    
+*/
+/*  포인터 배열
+    int a = 10, b = 20, c = 30;
+    int *arr[10] = {&a, &b, &c};
+    const char *str[10] = {
+        "abc", 
+        "abc", 
+        "abc"
+    };
+*/
+/*  함수 포인터/void 포인터
+    //함수 포인터
+    int (*fget_Min)(int *arr, int n);
+    int *(*fint_Sort)(int *arr, int n);
+    fget_Min = get_Min;
+    fint_Sort = int_Sort;
+
+    //void 포인터
+    int n1 = 10;
+    float n2 = 3.5f;
+    char c = 'a';
+    void *ptr;
+    ptr = &n1; //n1의 메모리 주소를 void 포인터 ptr에 저장
+    printf("%d\n", *(int *)ptr); //void 포인터를 int 포인터로 변환한 뒤 역참조
+    ptr = &n2; //n2의 메모리 주소를 void 포인터 ptr에 저장
+    printf("%f\n", *(float *)ptr); // void 포인터를 float 포인터로 변환한 뒤 역참조
+    ptr = &c; //c의 메모리 주소를 void 포인터 ptr에 저장
+    printf("%c\n", *(char *)ptr); //void 포인터를 char 포인터로 변환한 뒤 역참조
+*/
+/*  구조체 : 독립적인 데이터 공간, 구조체 배열, 구조체 포인터
+
+    struct person p[5] = {};
+    for(int i = 0; i < 5; i++){
+        printf("name : %s\n", p[i].name);
+        printf("phone : %s\n", p[i].phone);
+        printf("age : %d\n\n", p[i].age);
+    }
+
+    person pm = {"qoansrb", "01023328874", 29};
+    person *pMan;
+    pMan = &pm;
+    //구조체 변수
+    printf("name : %s\n", pm.name);
+    printf("phone : %s\n", pm.phone);
+    printf("age : %d\n\n", pm.age);
+    //구조체 포인터1
+    printf("name : %s\n", (*pMan).name);
+    printf("phone : %s\n", (*pMan).phone);
+    printf("age : %d\n\n", (*pMan).age);
+    //구조체 포인터2
+    printf("name : %s\n", pMan->name);
+    printf("phone : %s\n", pMan->phone);
+    printf("age : %d\n\n", pMan->age);
+*/
+/*  공용체(유니언) : 메모리 공간 공유
+
+    union data;
+*/
+/*  열거형
+
+    week w;
+*/
     return 0;
 }
