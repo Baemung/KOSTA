676ed03d10f355fa024994d8c0af8ae0b7d926d0
Mungyu Bae <anvo930524@gmail.com>
2021-03-25 17:28:55 +0900

    4w4d

--- DB/DBManager/DBManager.cs
+++ DB/DBManager/DBManager.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DBManager
@@ -54,6 +49,14 @@ namespace DBManager
         private void mnuFileOpendb_Click(object sender, EventArgs e)
         {
             //DB File Open
+            sqlConn.Close();
+            SQLInput.Text = "";
+            DataGrid.Columns.Clear();
+            DataGrid.Rows.Clear();
+            DataGrid.Refresh();
+            slSQLReturn.Text = "";
+            ddTableList.Text = "Table List";
+            ddTableList.DropDownItems.Clear();
             try
             {
                 DialogResult ret = openFileDialog.ShowDialog();
@@ -63,21 +66,63 @@ namespace DBManager
 
                 sqlCmd.Connection = sqlConn;
                 sqlConn.ConnectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={fn};Integrated Security=True;Connect Timeout=30";
-                sqlConn.Open(); 
-                DBOpenStatus.Text = $"  Open {sfn} File Success  ";
-                DBOpenStatus.BackColor = Color.LawnGreen;
+                sqlConn.Open();
+                
+                slDBOpenStatus.Text = $"  {sfn}  ";
+                slDBOpenStatus.BackColor = Color.LawnGreen;
+
+                DataTable dt = sqlConn.GetSchema("Tables");
+                for(int i = 0; i < dt.Rows.Count; i++)
+                {
+                    ddTableList.DropDownItems.Add(dt.Rows[i].ItemArray[2].ToString());
+                }
             }
-            catch(SqlException e1)
+            catch (SqlException e1)
             {
                 MessageBox.Show(e1.Message);
-                DBOpenStatus.Text = "  Open DB File Fail  ";
-                DBOpenStatus.BackColor = Color.Red;
+                slDBOpenStatus.Text = "  Open DB File Fail  ";
+                slDBOpenStatus.BackColor = Color.Red;
+            }
+        }
+
+        private void mnuFileClose_Click(object sender, EventArgs e)
+        {
+            sqlConn.Close();
+            slDBOpenStatus.Text = $"  DB Disconnected  ";
+            slDBOpenStatus.ForeColor = Color.Red;
+            slDBOpenStatus.BackColor = Color.FromArgb(0,0,0,0);
+            slSQLReturn.Text = "";
+            ddTableList.Text = "";
+            ddTableList.DropDownItems.Clear();
+        }
+
+        private void mnuFileUpdate_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < DataGrid.Rows.Count; i++)
+            {
+                for(int j = 0; j < DataGrid.Columns.Count; j++)
+                {
+                    string ed = DataGrid.Rows[i].Cells[j].ToolTipText;
+                    if (ed == "Edited") // update [테이블명] set [칼럼명]=[바꾸려는 값] where [조건; 1st_colname(key) = 1st_colvalue]
+                    {
+                        string tn = tName;
+
+                        string fn = DataGrid.Columns[j].HeaderText;
+                        object cv = DataGrid.Rows[i].Cells[j].Value;
+
+                        string f1 = DataGrid.Columns[0].HeaderText; // (일반적으로) 첫번째 칼럼이 pk이기 때문.
+                        object c1 = DataGrid.Rows[i].Cells[0].Value;
+
+                        string sql = $"update {tn} set {fn} = '{cv}' where {f1} = {c1}";
+                        RunSQL(sql);
+                    }
+                }
             }
         }
 
         private void mnuExecSql_Click(object sender, EventArgs e)
         {
-            RunSQL(SQLInput.Text); 
+            RunSQL(SQLInput.Text);
         }
 
         private void mnuExecSelsql_Click(object sender, EventArgs e)
@@ -85,33 +130,157 @@ namespace DBManager
             RunSQL(SQLInput.SelectedText);
         }
 
+        private string Get_Token(char deli, string str, int index)
+        {
+            string[] strs = str.Split(deli);
+            string ret = strs[index];
+            return ret;
+        }
+
+        string tName;
         int RunSQL(string sql)
         {
             try
             {
                 sqlCmd.CommandText = sql;
-                sqlCmd.ExecuteNonQuery();   // Select 문 제외 -> No return value
-                                            // Update, insert, delete, create, alter
-                // sqlCmd.ExecuteReader();
+                if (Get_Token(' ', sql.Trim(), 0).ToUpper() == "SELECT")
+                {
+                    DataGrid.Columns.Clear();
+                    DataGrid.Rows.Clear();
+                    DataGrid.Refresh();
 
-                SQLreturn.Text = "  Execution Success  ";
-                SQLreturn.BackColor = Color.LawnGreen; ;
+                    bool is_title = true;
+                    tName = Get_Token(' ', sql.Trim(), 3);
+                    SqlDataReader sr = sqlCmd.ExecuteReader();
+
+                    for (int i = 0; sr.Read(); i++) //NULL이 나올때 까지 한줄씩 읽음
+                    {
+                        string buf = "";
+                        for (int j = 0; j < sr.FieldCount; j++)
+                        {
+                            if (is_title)
+                            {
+                                string title = sr.GetName(j);
+                                DataGrid.Columns.Add(title, title);
+                            }
+                            object str = sr.GetValue(j);
+                            buf += $"{str},";
+                        }
+                        is_title = false;
+                        string[] items = buf.Split(',');
+                        DataGrid.Rows.Add(items);
+                    }
+                    sr.Close();
+                }
+                else
+                {
+                    sqlCmd.ExecuteNonQuery();   // Select 문 제외 -> No return value
+                                                // Update, insert, delete, create, alter
+                }
+                slSQLReturn.Text = "  Execution Success  ";
+                slSQLReturn.BackColor = Color.LawnGreen; ;
             }
             catch (SqlException e1)
             {
                 MessageBox.Show(e1.Message);
 
-                SQLreturn.Text = "  Execution Fail  ";
-                SQLreturn.BackColor = Color.Red;
+                slSQLReturn.Text = "  Execution Error  ";
+                slSQLReturn.BackColor = Color.Red;
             }
             catch (InvalidOperationException e2)
             {
                 MessageBox.Show(e2.Message);
 
-                SQLreturn.Text = "  Execution Fail  ";
-                SQLreturn.BackColor = Color.Red;
+                slSQLReturn.Text = "  Execution Error  ";
+                slSQLReturn.BackColor = Color.Red;
+            }
+            catch (FileNotFoundException e3)
+            {
+                MessageBox.Show(e3.Message);
+
+                slSQLReturn.Text = "  Execution Error  ";
+                slSQLReturn.BackColor = Color.Red;
             }
             return 0;
+        }
+
+        private void SQLInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (mnuExecEnter.Checked && e.KeyCode == Keys.Enter)
+            {
+                string last_line = string.Empty;
+                string[] lines = SQLInput.Text.Split('\r','\n');
+                for(int i = 0; i < lines.Length; i++)
+                {
+                    if(lines[i].Trim().Length > 0)
+                    {
+                        last_line = lines[i].Trim();
+                    }
+                }
+                RunSQL(last_line);
+            }
+        }
+
+        private void DataGrid_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            DataGrid.Rows[e.RowIndex].Cells[e.ColumnIndex].ToolTipText = "Edited";
+        }
+
+        private void ddTableList_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            ddTableList.Text = e.ClickedItem.Text;
+            string sql = $"select * from {ddTableList.Text}";
+            tName = ddTableList.Text;
+            SQLInput.Text = sql;
+            RunSQL(sql);
+        }
+
+        string newCtype;
+        private void mnuEditAddcol_Click(object sender, EventArgs e)
+        {
+            addCol addC = new addCol();
+            DialogResult dlg = addC.ShowDialog();
+            if (dlg == DialogResult.Cancel) return;
+            newCtype = addC.cType;
+            DataGrid.Columns.Add(addC.cName, addC.cName);
+        }
+        private void mnuEditAddrow_Click(object sender, EventArgs e)
+        {
+            DataGrid.Rows.Add();
+        }
+
+        private void mnuEditNew_Click(object sender, EventArgs e)
+        {
+            title newTable = new title();
+            if (newTable.ShowDialog() == DialogResult.Cancel) return;
+
+            //Create table
+            string new_tName = newTable.new_tName;
+            string cols = "";
+            for (int i = 0; i < DataGrid.Columns.Count; i++)
+            {
+                cols += $"{DataGrid.Columns[i].HeaderText} varchar(20),";
+            }
+            cols = cols.Substring(0, cols.Length - 1);
+            RunSQL($"create table {new_tName} ({cols})");
+
+            //Insert
+            for (int i = 0; i < DataGrid.Rows.Count-1; i++)
+            {
+                string rows = "";
+                for (int j = 0; j < DataGrid.Columns.Count; j++)
+                {
+                    object row = DataGrid.Rows[i].Cells[j].Value;
+                    rows += $"{row},";
+                }
+                rows = rows.Substring(0, rows.Length - 1);
+                RunSQL($"insert into {new_tName} values ({rows})");
+            }
+        }
+
+        private void mnuExecEnter_Click(object sender, EventArgs e)
+        {
+            mnuExecEnter.Checked = !mnuExecEnter.Checked;
         }
     }
 }
---
+++ DB/DBManager/input.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace DBManager
+{
+    partial class input
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbInput = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Location = new System.Drawing.Point(37, 59);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(32, 12);
+            this.lbInput.TabIndex = 0;
+            this.lbInput.Text = "Input";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(99, 55);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(181, 21);
+            this.tbInput.TabIndex = 1;
+            this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
+            // 
+            // input
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 138);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.lbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "input";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "input";
+            this.Load += new System.EventHandler(this.input_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.TextBox tbInput;
+    }
+}
