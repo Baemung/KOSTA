8095efbf0487e224ecc431d3b4bfb5aa6a061029
unknown <anvo930524@gmail.com>
2021-05-03 17:41:08 +0900

    10w1d

---
+++ ASP/Basic/Basic/Context/Users.cs
@@ -0,0 +1,16 @@
+﻿using Basic.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Basic.Context
+{
+    public class Users : DbContext
+    {
+        public Users() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\KOSTA\Desktop\KOSTA\ASP\ASP_DB.mdf;Integrated Security=True;Connect Timeout=30")
+        { }
+        public DbSet<User> UserInfo { get; set; }
+    }
+}
---
+++ ASP/Basic/Basic/Models/Fac.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Basic.Models
+{
+    public class Fac
+    {
+        public int id { get; set; }
+        public string fCode { get; set; }
+        public string fName { get; set; }
+        public string fDesc { get; set; }
+    }
+}
